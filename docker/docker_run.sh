d776cf921a114d529937c2d2f7812fbd7ee13e54
Henrik <hflemmen@gmail.com>
2023-01-12 11:23:12 +0100

    Changed image to display

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,2 +1,6 @@
+xhost +local:root
 #docker run -it --network host -v /home/henrik/Data/oxford:/oxford/ -v /home/henrik/Results/yeti:/res/   keenan-yeti-1.0 "cd res &&  python3 /catkin_ws/src/yeti_radar_odometry/scripts/evaluate_odometry.py"
-docker run -it --network host -v /home/henrik/Data/oxford:/oxford/ -v /home/henrik/Results/yeti:/res/   keenan-yeti-1.0 "cd res &&  /catkin_ws/build/yeti/odometry --root /oxford/ --sequence polarlys --append _polarlys_test && cat accuracytest.csv"
+docker run -it --network host --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+ -v /home/henrik/Data/oxford:/oxford/ -v /home/henrik/Results/yeti:/res/ \
+   keenan-yeti-1.0 "cd res &&  /catkin_ws/build/yeti/odometry --root /oxford/ --sequence polarlys --append _polarlys_test && cat accuracytest.csv"
