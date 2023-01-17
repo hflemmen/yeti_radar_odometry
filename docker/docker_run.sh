40e4b691b5253f95486fce0ed95db6885b199349
Henrik <hflemmen@gmail.com>
2023-01-17 11:17:35 +0100

    Minor changes. Now it can show images.

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -3,4 +3,8 @@ xhost +local:root
 docker run -it --network host --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" \
 --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
  -v /home/henrik/Data/oxford:/oxford/ -v /home/henrik/Results/yeti:/res/ \
-   keenan-yeti-1.0 "cd res &&  /catkin_ws/build/yeti/odometry --root /oxford/ --sequence polarlys --append _polarlys_test && cat accuracytest.csv"
+   keenan-yeti-1.0 "cd res &&  /catkin_ws/build/yeti/odometry --root /oxford/ --sequence 2019-01-10-14-36-48-radar-oxford-10k-partial --append test && cat accuracytest.csv"
+#docker run -it --network host --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" \
+#--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+# -v /home/henrik/Data/oxford:/oxford/ -v /home/henrik/Results/yeti:/res/ \
+#   keenan-yeti-1.0 "cd res &&  /catkin_ws/build/yeti/odometry --root /oxford/ --sequence polarlys --append _polarlys_test && cat accuracytest.csv"
